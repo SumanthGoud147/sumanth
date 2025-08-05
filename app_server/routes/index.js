e78b1ec0f4709badf5b2dad0cb3d3908e8db259f
Sumanth <23eg107d24@anurag.edu.in>
2025-08-05 15:03:38 +0530

    3rd commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+const express = require('express');
+const router = express.Router();
+const ctrlMain = require('/controllers/');
+/* GET homepage. */
+router.get('/' ,ctrlMain.index);
 module.exports = router;
